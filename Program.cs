8220bb74841dc9243d449eb2d9d955582caa8cbe
Maged Eshak <magedeshak36@gmail.com>
2025-05-04 16:25:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5_soulution_ITI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
